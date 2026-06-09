116c131807ba36e8510bc703209725192fdae80c
Kaveri Borude <kaveriborude@outlook.com>
2026-06-09 14:27:14 +0530

    Add new feature

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // new feature added
+
+
+// new js file create kei means new feature 
+// // 
+// kaver@Kaveri MINGW64 /c/Kaveri/Apna College/gitDemo/project2 (feature)
+// $ git status
+// On branch feature
+// Changes not staged for commit:
+//   (use "git add <file>..." to update what will be committed)
+//   (use "git restore <file>..." to discard changes in working directory)
+//         modified:   index.html
+
+// Untracked files:
+//   (use "git add <file>..." to include in what will be committed)
+//         app.js
+
+// no changes added to commit (use "git add" and/or "git commit -a")
+
+// kaver@Kaveri MINGW64 /c/Kaveri/Apna College/gitDemo/project2 (feature)
+// $ git add .
+
+// kaver@Kaveri MINGW64 /c/Kaveri/Apna College/gitDemo/project2 (feature)
+// $ git commit -m " Add new feature "
+// [feature b47f6f4]  Add new feature
+//  2 files changed, 76 insertions(+)
+//  create mode 100644 app.js
+
+// kaver@Kaveri MINGW64 /c/Kaveri/Apna College/gitDemo/project2 (feature)
+// $ git status
+// On branch feature
+// nothing to commit, working tree clean
