cf486e5461f5040ea165666ee85f95892b5a0739
Kaveri Borude <kaveriborude@outlook.com>
2026-06-09 15:24:17 +0530

    add new button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// new feature added
+// new feature added - button
 
 
 // new js file create kei means new feature 
@@ -30,3 +30,33 @@
 // $ git status
 // On branch feature
 // nothing to commit, working tree clean
+
+
+// _____________  to accept github repo to local system _________________
+
+
+// kaver@Kaveri MINGW64 /c/Kaveri/Apna College/gitDemo/project2 (feature)
+// $ git branch
+// * feature
+//   main
+
+// kaver@Kaveri MINGW64 /c/Kaveri/Apna College/gitDemo/project2 (feature)
+// $ git checkout main
+// Switched to branch 'main'
+// Your branch is up to date with 'origin/main'.
+
+// kaver@Kaveri MINGW64 /c/Kaveri/Apna College/gitDemo/project2 (main)
+// $ git pull origin  main
+// remote: Enumerating objects: 1, done.
+// remote: Counting objects: 100% (1/1), done.
+// remote: Total 1 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+// Unpacking objects: 100% (1/1), 904 bytes | 150.00 KiB/s, done.
+// From https://github.com/Kaveri-Borude/project2
+//  * branch            main       -> FETCH_HEAD
+//    c199f16..d0a6940  main       -> origin/main
+// Updating c199f16..d0a6940
+// Fast-forward
+//  app.js     | 32 +++++++++++++++++++++++++++
+//  index.html | 75 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//  2 files changed, 107 insertions(+)
+//  create mode 100644 app.js
